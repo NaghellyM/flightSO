7cc669c2f1e917fc273a38877d471dab8c05bf85
NaghellyM <naghellymoreno0610@gmail.com>
2024-10-31 07:15:35 -0500

    feat:Creation of bios, login and desk modules

---
+++ src/app/components/desk/desk.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desk',
+  standalone: true,
+  imports: [],
+  templateUrl: './desk.component.html',
+  styleUrl: './desk.component.css'
+})
+export class DeskComponent {
+
+}
