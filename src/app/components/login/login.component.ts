29f5570cea4c51448dcd1e21caa6fcb5aa29dd0b
NaghellyM <naghellymoreno0610@gmail.com>
2024-11-27 08:50:42 -0500

    feat: initial configuration to connect to the serve

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { User } from '../../models/user.models';
+import { SecurityService } from '../../services/security.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  constructor(private router: Router) {}
-  login = {
-    name: 'User 1',
-    image: './assets/images/avatar_flightSO.png'
+export class LoginComponent implements OnInit,OnDestroy{ 
+  user: User;
+
+  constructor(private SecurityService: SecurityService, private router: Router) {
+    this.user = { email: "", password: "" };
+  }
+
+  ngOnInit() {}
+  ngOnDestroy() {}
+
+  login() {
+    this.SecurityService.login(this.user).subscribe({
+      next: (data) => {
+        this.SecurityService.saveSession(data);
+        this.router.navigate(["dashboard"]);
+      },
+      error: (error) => {
+        Swal.fire(
+          "Autenticación Inválida",
+          "Usuario o contraseña inválido",
+          "error"
+        );
+      },
+    });
   }
   goToDesk(): void {
     this.router.navigate(['/desk']);
