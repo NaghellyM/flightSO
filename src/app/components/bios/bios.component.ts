5b65b1c0b50f28d6fcc7f97ebf6d3c36cade86f5
NaghellyM <naghellymoreno0610@gmail.com>
2024-11-28 18:22:09 -0500

    feat:logical creation of bios

--- src/app/components/bios/bios.component.ts
+++ src/app/components/bios/bios.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-bios',
+  selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './bios.component.html',
-  styleUrl: './bios.component.css'
+  styleUrl: './bios.component.css',
 })
-export class BiosComponent {
+export class BiosComponent implements OnInit {
+  constructor(private router: Router, private ngZone: NgZone) {}
 
+  biosText: string[] = ['Initializing BIOS...'];
+  private biosData: string[] = [
+    'Checking system memory...',
+    'Memory Test Passed: 16384MB OK',
+    'Checking CPU...',
+    'CPU Test Passed: Intel Core i7 3.2GHz',
+    'Detecting hard drives...',
+    'Drive 0: SSD 500GB OK',
+    'Drive 1: HDD 1TB OK',
+    'Booting OS...',
+    'System ready.',
+  ];
+
+  ngOnInit(): void {
+    this.simulateBIOS();
+  }
+
+  simulateBIOS(): void {
+    let i = 0;
+    this.ngZone.runOutsideAngular(() => {
+      const interval = setInterval(() => {
+        if (i < this.biosData.length) {
+          this.ngZone.run(() => {
+            this.biosText = [...this.biosText, this.biosData[i]];
+            i++;
+          });
+        } else {
+          clearInterval(interval);
+          this.router.navigate(['/login']);
+        }
+      }, 1000);
+    });
+  }
 }
